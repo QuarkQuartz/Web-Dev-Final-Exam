6a48048834936862dbf5e29b71115b8ce3cbf8cb
QuarkQuartz <jqadolfo40@gmail.com>
2023-06-21 19:25:50 +0800

    hi sir

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
+let passwords = [];
 document.addEventListener('DOMContentLoaded', function() {
-    let passwords = [];
-  
-    function displayPasswords() {
-      const passwordTable = document.getElementById('password-table');
-      const passwordTableBody = passwordTable.querySelector('tbody');
-      passwordTableBody.innerHTML = '';
-  
-      passwords.forEach(function(password, index) {
-        const row = document.createElement('tr');
-        row.innerHTML = `
-          <td>${password.label}</td>
-          <td>${password.password}</td>
-          <td>
-            <button class="btn btn-primary btn-sm edit-password" data-index="${index}" data-toggle="modal" data-target="#editModal">Edit</button>
-            <button class="btn btn-danger btn-sm delete-password" data-index="${index}">Delete</button>
-          </td>
-        `;
-        passwordTableBody.appendChild(row);
-      });
-    }
-  
-    function savePasswords() {
-      localStorage.setItem('passwords', JSON.stringify(passwords));
-    }
-  
-    function addPassword(label, password) {
-      const newPassword = {
-        label: label,
-        password: password
-      };
-  
-      passwords.push(newPassword);
-      savePasswords();
-    }
-  
-    function editPassword(index, label, password) {
-      passwords[index].label = label;
-      passwords[index].password = password;
-      savePasswords();
-    }
-  
-    function deletePassword(index) {
-      passwords.splice(index, 1);
-      savePasswords();
-    }
-  
-    document.getElementById('password-form').addEventListener('submit', function(e) {
-      e.preventDefault();
-  
-      const labelInput = document.getElementById('label');
-      const passwordInput = document.getElementById('password');
-  
-      addPassword(labelInput.value, passwordInput.value);
-  
-      labelInput.value = '';
-      passwordInput.value = '';
-  
-      displayPasswords();
+
+  function displayPasswords() {
+    const passwordTableBody = document.querySelector('#password-table tbody');
+    passwordTableBody.innerHTML = '';
+
+    passwords.forEach(function(password, index) {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${password.label}</td>
+        <td>${password.password}</td>
+        <td>
+          <button class="btn btn-primary btn-sm edit-password" data-bs-toggle="modal" data-bs-target="#editModal" data-index="${index}">Edit</button>
+          <button class="btn btn-danger btn-sm delete-password" data-index="${index}">Delete</button>
+        </td>
+      `;
+      passwordTableBody.appendChild(row);
     });
+  }
+
+  function savePasswords() {
+    localStorage.setItem('passwords', JSON.stringify(passwords));
+  }
+
+  function addPassword(label, password) {
+    const newPassword = {
+      label: label,
+      password: password
+    };
   
-    $('#editModal').on('show.bs.modal', function(e) {
-      const button = e.relatedTarget;
-      const index = button.dataset.index;
+    passwords.push(newPassword);
+    savePasswords();
+  }
+
+  function editPassword(index, label, password) {
+    passwords[index].label = label;
+    passwords[index].password = password;
+    savePasswords();
+  }
+
+  function deletePassword(index) {
+    passwords.splice(index, 1);
+    savePasswords();
+  }
+
+  document.getElementById('password-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const labelInput = document.getElementById('label');
+    const passwordInput = document.getElementById('password');
+  
+    addPassword(labelInput.value, passwordInput.value);
+  
+    labelInput.value = '';
+    passwordInput.value = '';
+  
+    displayPasswords();
+  });
+
+  document.getElementById('password-table').addEventListener('click', function(e) {
+    if (e.target.classList.contains('edit-password')) {
+      const index = e.target.dataset.index;
       const password = passwords[index];
   
       const editLabelInput = document.getElementById('edit-label');
@@ -72,37 +71,64 @@ document.addEventListener('DOMContentLoaded', function() {
   
       const saveChangesButton = document.getElementById('save-changes');
       saveChangesButton.dataset.index = index;
-    });
-  
-    document.getElementById('save-changes').addEventListener('click', function() {
-      const index = this.dataset.index;
-      const editLabelInput = document.getElementById('edit-label');
-      const editPasswordInput = document.getElementById('edit-password');
-  
-      editPassword(index, editLabelInput.value, editPasswordInput.value);
-  
-      editLabelInput.value = '';
-      editPasswordInput.value = '';
-      $('#editModal').modal('hide');
-  
+    } else if (e.target.classList.contains('delete-password')) {
+      const index = e.target.dataset.index;
+      deletePassword(index);
       displayPasswords();
-    });
-  
-    document.getElementById('password-table').addEventListener('click', function(e) {
-      if (e.target.classList.contains('delete-password')) {
-        const index = e.target.dataset.index;
-        deletePassword(index);
-  
-        displayPasswords();
-      }
-    });
-  
-    // Initialize passwords from local storage
-    const storedPasswords = localStorage.getItem('passwords');
-    if (storedPasswords) {
-      passwords = JSON.parse(storedPasswords);
     }
-  
+  });
+
+  const editModal = new bootstrap.Modal(document.getElementById('editModal'));
+  document.getElementById('editModal').addEventListener('show.bs.modal', function(e) {
+    const button = e.relatedTarget;
+    const index = button.dataset.index;
+    const password = passwords[index];
+
+    const editLabelInput = document.getElementById('edit-label');
+    const editPasswordInput = document.getElementById('edit-password');
+
+    editLabelInput.value = password.label;
+    editPasswordInput.value = password.password;
+
+    const saveChangesButton = document.getElementById('save-changes');
+    saveChangesButton.dataset.index = index;
+  });
+
+  document.getElementById('save-changes').addEventListener('click', function() {
+    const index = this.dataset.index;
+    const editLabelInput = document.getElementById('edit-label');
+    const editPasswordInput = document.getElementById('edit-password');
+
+    editPassword(index, editLabelInput.value, editPasswordInput.value);
+
+    editLabelInput.value = '';
+    editPasswordInput.value = '';
+    editModal.hide();
+
     displayPasswords();
   });
-  
+
+  document.getElementById('password-table').addEventListener('click', function(e) {
+    if (e.target.classList.contains('edit-password')) {
+      const index = e.target.dataset.index;
+      const password = passwords[index];
+
+      const editLabelInput = document.getElementById('edit-label');
+      const editPasswordInput = document.getElementById('edit-password');
+
+      editLabelInput.value = password.label;
+      editPasswordInput.value = password.password;
+
+      const saveChangesButton = document.getElementById('save-changes');
+      saveChangesButton.dataset.index = index;
+    }
+  });
+
+  // Initialize passwords from local storage
+  const storedPasswords = localStorage.getItem('passwords');
+  if (storedPasswords) {
+    passwords = JSON.parse(storedPasswords);
+  }
+
+  displayPasswords();
+});
